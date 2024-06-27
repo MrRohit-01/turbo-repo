85ec2199e734a375610e0d6ad53fc16ee77b46c5
Rohit Kumar Barada <rohitbarada.com>
2024-06-27 19:20:56 +0530

    update

---
+++ packages/ui/src/admin.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/button";
+
+export function Admin(){
+    return (
+        <div>
+            <Button children="data" appName="hello"/>
+            aflmf
+        </div>
+    )
+}
